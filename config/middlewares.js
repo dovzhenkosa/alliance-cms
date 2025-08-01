8160d0baf954bef168f3945576cb40275ba90cea
stanislavdovzhenko <dovzhenkosa@gmail.com>
2025-08-02 00:25:11 +0300

    add security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,22 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.SUPABASE_URL || '',
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
