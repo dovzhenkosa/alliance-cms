28de0ad4f6390395a7844c2485fbaee5096b665c
stanislavdovzhenko <dovzhenkosa@gmail.com>
2025-08-01 23:21:06 +0300

    add supabase bucket

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'supabase',
+            providerOptions: {
+                apiUrl: env('SUPABASE_URL'),
+                apiKey: env('SUPABASE_PUBLIC_KEY'),
+                bucket: env('SUPABASE_BUCKET'),
+                options: {},
+            },
+        },
+    },
+});
